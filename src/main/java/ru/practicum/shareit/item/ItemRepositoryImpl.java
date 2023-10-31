1f7255dd9d9932153ef03788bc9ac826ce3a7683
Ksenni888 <len4244@yandex.ru>
2023-10-31 07:38:23 +0300

    error correction 4 style

--- src/main/java/ru/practicum/shareit/item/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepositoryImpl.java
@@ -17,12 +17,14 @@ public class ItemRepositoryImpl implements ItemRepository {
 
     private long itemId;
 
+    @Override
     public Item create(long userId, Item item) {
         item.setId(incrementId());
         items.put(item.getId(), item);
         return item;
     }
 
+    @Override
     public Item update(long userId, Item item, long itemId) {
 
         if (item.getAvailable() != null) {
@@ -34,26 +36,30 @@ public class ItemRepositoryImpl implements ItemRepository {
         if (item.getDescription() != null) {
             items.get(itemId).setDescription(item.getDescription());
         }
-       return items.get(itemId);
+        return items.get(itemId);
     }
 
+    @Override
     public Item findById(long userId, long itemId) {
         return items.get(itemId);
     }
 
+    @Override
     public List<Item> getItemsByUserId(long userId) {
         return items.values().stream().filter(x -> x.getOwner().getId() == userId).collect(Collectors.toList());
     }
 
+    @Override
     public List<Item> findItems(long userId, String text) {
         return items.values().stream().filter(x -> x.getName().toLowerCase().contains(text.toLowerCase()) || x.getDescription().toLowerCase().contains(text.toLowerCase()) && (x.getAvailable())).collect(Collectors.toList());
     }
 
-    private long incrementId() {
-        return ++itemId;
-    }
-
+    @Override
     public boolean containsItem(long itemId) {
         return items.containsKey(itemId);
+    }
+
+    private long incrementId() {
+        return ++itemId;
     }
 }
