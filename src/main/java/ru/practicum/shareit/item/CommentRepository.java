f3bf96404a864df4d9d7061ec5919478b911db5f
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-11-21 00:04:27 +0300

    change mappings s1

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Comments;
-import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
