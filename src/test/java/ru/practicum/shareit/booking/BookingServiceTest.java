2efccc28edfd22c9c7586e65e611a5f4bfa46d3f
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-04 22:14:33 +0300

    BookingServiceTest all test

--- src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
@@ -7,6 +7,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
@@ -20,6 +23,8 @@ import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -113,6 +118,71 @@ public class BookingServiceTest {
     }
 
     @Test
-    public void getBookingTest(long userId, long bookingId) {}
+    public void getBookingTest() {
+        User user = new User();
+        user.setId(1L);
 
+        User user2 = new User();
+        user2.setId(2L);
+
+        Item item = new Item();
+        item.setId(1L);
+        item.setOwner(user);
+        item.setAvailable(true);
+
+        Booking saveBooking = new Booking();
+        saveBooking.setId(1L);
+        saveBooking.setBooker(user2);
+        saveBooking.setItem(item);
+        saveBooking.setStatus(BookingStatus.WAITING);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(bookingRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(bookingRepository.findById(1L)).thenReturn(Optional.of(saveBooking));
+
+        Booking result = bookingService.getBooking(1L, 1L);
+        Assertions.assertEquals(saveBooking, result);
     }
+
+    @Test
+    public void getBookingsByStatusTest() {
+        List<Booking> bookingsByUserId = new ArrayList<>();
+
+        User user = new User();
+        user.setId(1L);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(bookingRepository.findByBookerId(1L, PageRequest.of(0, 1,
+                Sort.by("start").descending()))).thenReturn(bookingsByUserId);
+
+        List<Booking> allBookings = bookingService.checkState(bookingsByUserId, "ALL");
+
+        List<Booking> result = bookingService.getBookingsByStatus(1L, "ALL", PageRequest.of(0, 1,
+                Sort.by("start").descending()));
+
+        Assertions.assertEquals(allBookings, result);
+    }
+
+    @Test
+    public void getUserBookingsTest() {
+        Item item = new Item();
+        item.setId(1L);
+        Item item1 = new Item();
+        item1.setId(2L);
+        List<Item> itemByOwnerId = List.of(item, item1);
+
+        Mockito.when(itemRepository.findByOwnerId(1L)).thenReturn(itemByOwnerId);
+        List<Booking> saveBooking = new ArrayList<>();
+        List<Long> allItemsByUser = List.of(1L, 2L);
+
+        Mockito.when(bookingRepository.findByItemIdIn(allItemsByUser, PageRequest.of(0, 1,
+                Sort.by("start").descending()))).thenReturn(saveBooking);
+
+        List<Booking> allBooking = bookingService.checkState(saveBooking, "ALL");
+        List<Booking> result = bookingService.getUserBookings(1L, "ALL", PageRequest.of(0, 1,
+                Sort.by("start").descending()));
+
+        Assertions.assertEquals(allBooking, result);
+    }
+
+}
