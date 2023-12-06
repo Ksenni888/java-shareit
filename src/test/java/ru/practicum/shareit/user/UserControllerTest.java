dde6643a8ca2a944d7890701c9fcdea6aeaed1a7
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-06 04:00:23 +0300

    UserControllerTest 3

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,127 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.practicum.shareit.user.controller.UserController;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+    @Autowired
+    @Mock
+    private UserMapper userMapper;
+    //  @Mock
+    // private UserRepository userRepository;
+    @InjectMocks
+    UserController userController;
+
+    private MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .build();
+    }
+
+    @Test
+    public void createTest() throws Exception {
+
+        UserDto userDto1 = UserDto.builder()
+                .id(1L)
+                .name("name")
+                .email("email@mail.ru")
+                .build();
+
+        when(userService.create(any())).thenReturn(userDto1);
+
+        mockMvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto1))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(userDto1.getName()))
+                .andExpect(jsonPath("$.email").value(userDto1.getEmail()));
+    }
+
+    @Test
+    public void updateTest() throws Exception {
+
+        UserDto userDto1 = UserDto.builder()
+                .id(2L)
+                .name("name1")
+                .email("email@mail.ru")
+                .build();
+
+        when(userService.update(any(), anyLong())).thenReturn(userDto1);
+
+        mockMvc.perform(patch("/users/{userId}", 2L)
+                        .content(mapper.writeValueAsString(userDto1))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.name").value(userDto1.getName()));
+    }
+
+    @Test
+    public void getAllTest() throws Exception {
+
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .name("name")
+                .email("email")
+                .build();
+
+        List<UserDto> saveUserDto = List.of(userDto);
+
+        when(userService.getAll())
+                .thenReturn(saveUserDto);
+
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value(saveUserDto.get(0).getId()))
+                .andExpect(jsonPath("$[0].name").value(saveUserDto.get(0).getName()))
+                .andExpect(jsonPath("$[0].email").value(saveUserDto.get(0).getEmail()));
+    }
+
+
+}
