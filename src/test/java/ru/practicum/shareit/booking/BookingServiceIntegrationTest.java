4a3666e0f5f3aa24f540f4c12a4a2ce33d67014e
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-07 05:13:01 +0300

    Integration tests st

--- src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
@@ -1,13 +1,9 @@
 package ru.practicum.shareit.booking;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
--- src/test/java/ru/practicum/shareit/request/ItemRequestServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestServiceIntegrationTest.java
@@ -1,12 +1,9 @@
 package ru.practicum.shareit.request;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
