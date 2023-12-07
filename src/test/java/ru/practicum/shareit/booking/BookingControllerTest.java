ea378366ff627d07a9dbea67423963a26e135000
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-07 04:14:59 +0300

    BookingControllerTest

---
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -0,0 +1,88 @@
+package ru.practicum.shareit.booking;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.practicum.shareit.booking.controller.BookingController;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.service.BookingServiceImpl;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class BookingControllerTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
+
+    private MockMvc mockMvc;
+    @Mock
+    private BookingServiceImpl bookingService;
+    @InjectMocks
+    private BookingController bookingController;
+
+    @BeforeEach
+    void setMockMvc() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(bookingController)
+                .build();
+    }
+
+    public static final User user = new User(1L, "userName", "email@mail.ru");
+    public static final Item item = new Item(1L, "itemName", "itemDescription", true, user, null);
+
+    @Test
+    public void createBookingTest() throws Exception {
+        String str = "2023-12-28 12:30";
+        String str1 = "2023-12-30 12:30";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime start = LocalDateTime.parse(str, formatter);
+        LocalDateTime end = LocalDateTime.parse(str1, formatter);
+
+
+        Booking booking = new Booking(1L, start,
+                end, item, user, BookingStatus.APPROVED);
+
+        String addBooking = createBookingDtoJson(1L, start, end);
+
+        Mockito.when(bookingService.createBooking(anyLong(), any())).thenReturn(booking);
+
+        mockMvc.perform(post("/bookings")
+                        .header("X-Sharer-User-Id", booking.getBooker().getId())
+                        .content(addBooking)
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$..start[0]").value(2023));
+    }
+
+    public static String createBookingDtoJson(long id, LocalDateTime start, LocalDateTime end) {
+        return "{\n" +
+                "    \"id\": \"" + id + "\",\n" +
+                "    \"start\": \"" + start + "\",\n" +
+                "    \"end\": \"" + end + "\"\n" +
+                "}";
+    }
+}
