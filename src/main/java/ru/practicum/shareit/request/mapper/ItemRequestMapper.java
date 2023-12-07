392f27ac2d9b894b0832c4ac6d21da7c9d38a110
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-07 22:53:08 +0300

    ItemRequestMapperTest add style

--- src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.request.mapper;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemDtoForRequest;
-import ru.practicum.shareit.request.dto.ItemRequestAll;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
@@ -26,15 +24,6 @@ public class ItemRequestMapper {
                 .build();
     }
 
-//    public ItemRequestAll toDtoRequestAll(ItemRequest itemRequest) {
-//        return ItemRequestAll.builder()
-//                .id(itemRequest.getId())
-//                .created(itemRequest.getCreated())
-//                .description(itemRequest.getDescription())
-//                .build();
-//
-//    }
-
     public ItemDtoForRequest toDtoItem(Item item) {
         return ItemDtoForRequest.builder()
                 .id(item.getId())
@@ -53,5 +42,4 @@ public class ItemRequestMapper {
                 .user(user)
                 .build();
     }
-
 }
