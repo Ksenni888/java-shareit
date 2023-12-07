51a925b8ce39e22b9e7e5d301ccd0c814e766284
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-07 15:04:15 +0300

    BookingRepositoryTest

---
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -0,0 +1,62 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.repository.BookingRepository;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class BookingRepositoryTest {
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void findByBookerIdTest() {
+
+        User user = new User(1L, "name", "mail@mail.ru");
+        User user2 = new User(1L, "name2", "mail2@mail.ru");
+        userRepository.save(user);
+        userRepository.save(user2);
+        Item item = new Item(1L, "name", "desc", true, user2, null);
+        itemRepository.save(item);
+
+        Booking booking = new Booking(1L, LocalDateTime.of(2023, Month.APRIL, 8, 12, 30),
+                LocalDateTime.of(2023, Month.APRIL, 10, 12, 30), item, user, BookingStatus.APPROVED);
+        bookingRepository.save(booking);
+
+        List<Booking> allUserBookings = List.of(booking);
+        List<Booking> result = bookingRepository.findByBookerId(user.getId(), PageRequest.of(0, 1,
+                Sort.by("start").descending()));
+
+        assertEquals(result.size(), 1);
+        assertEquals(result.get(0).getBooker().getId(), 1L);
+    }
+
+
+//    List<Booking> findByBookerId(long userId, Pageable pageable);
+//
+//    List<Booking> findByItemIdIn(List<Long> allItemsByUser, Pageable pageable);
+//
+//    List<Booking> findByItemIdAndStatus(long itemId, BookingStatus status);
+//
+//    List<Booking> findByBookerIdAndItemId(long userId, long itemId);
+
+}
