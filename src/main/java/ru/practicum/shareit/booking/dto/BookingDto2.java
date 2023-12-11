629ec9d9ab457258e546a7c307c7cde875ec02b9
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-11 21:50:01 +0300

    add Tests st2

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto2.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto2.java
@@ -1,20 +1,19 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-    @Setter
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class BookingDto2 {
-        private long id;
-        private LocalDateTime start;
-        private LocalDateTime end;
-        private Long bookerId;
-    }
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingDto2 {
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long bookerId;
+}
