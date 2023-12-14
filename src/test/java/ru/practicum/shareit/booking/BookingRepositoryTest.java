5a6208ed698a31103c7525e61510819cfe4fdcb7
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-14 16:53:09 +0300

    add Tests st4

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -28,53 +28,24 @@ public class BookingRepositoryTest {
     @Autowired
     private ItemRepository itemRepository;
 
-    private User user() {
-        User user = new User();
-        user.setId(1L);
-        user.setName("name");
-        user.setEmail("mail1@mail.ru");
-        return user;
-    }
-
-    private User user2() {
-        User user2 = new User();
-        user2.setId(2L);
-        user2.setName("name2");
-        user2.setEmail("mail2@mail.ru");
-        return user2;
-    }
-
-    private Item item() {
-        Item item = new Item();
-        item.setId(0L);
-        item.setName("name");
-        item.setDescription("desc");
-        item.setAvailable(true);
-        item.setOwner(user2());
-        item.setRequest(null);
-        return item;
-    }
-
-    private Booking booking() {
-        Booking booking = new Booking();
-        booking.setId(1L);
-        booking.setStart(LocalDateTime.of(2023, Month.APRIL, 8, 12, 30));
-        booking.setEnd(LocalDateTime.of(2023, Month.APRIL, 10, 12, 30));
-        booking.setItem(item());
-        booking.setBooker(user());
-        booking.setStatus(BookingStatus.WAITING);
-        return booking;
-    }
-
     @Test
     public void findByBookerIdTest() {
-        userRepository.save(user());
-        userRepository.save(user2());
-        itemRepository.save(item());
-        bookingRepository.save(booking());
-        List<Booking> result = bookingRepository.findByBookerId(1L, PageRequest.of(0, 1,
+
+        User user = new User(0L, "name", "mail@mail.ru");
+        User user2 = new User(0L, "name2", "mail2@mail.ru");
+        userRepository.save(user);
+        userRepository.save(user2);
+        Item item = new Item(0L, "name", "desc", true, user2, null);
+        itemRepository.save(item);
+
+        Booking booking = new Booking(0L, LocalDateTime.of(2023, Month.APRIL, 8, 12, 30),
+                LocalDateTime.of(2023, Month.APRIL, 10, 12, 30), item, user, BookingStatus.APPROVED);
+        bookingRepository.save(booking);
+
+        List<Booking> result = bookingRepository.findByBookerId(user.getId(), PageRequest.of(0, 1,
                 Sort.by("start").descending()));
+
         assertEquals(result.size(), 1);
-        assertEquals(result.get(0).getBooker().getId(), user().getId());
+        assertEquals(result.get(0).getBooker().getId(), user.getId());
     }
 }
