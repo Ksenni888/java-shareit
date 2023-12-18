6789b2b6b16ad08a609a4677c6440fc8bef6f3eb
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-18 05:46:14 +0300

    item controller done st1

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -3,12 +3,11 @@ package ru.practicum.shareit.request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
-import ru.practicum.shareit.item.dto.ItemDto;
+
 @Service
 public class ItemRequestClient extends BaseClient{
 
--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -3,24 +3,9 @@ package ru.practicum.shareit.request;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Sort;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.model.ItemRequest;
-//import ru.practicum.shareit.request.service.ItemRequestService;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/requests")
