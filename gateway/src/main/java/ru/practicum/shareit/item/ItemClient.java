6997359e37a4650aa6d506b9737182558d0b837d
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2023-12-18 05:41:30 +0300

    item controller done

--- gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
@@ -6,34 +6,49 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.Item;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.Map;
+
 @Service
-public class ItemClient extends BaseClient{
+public class ItemClient extends BaseClient {
 
-        private static final String API_PREFIX = "/items";
+    private static final String API_PREFIX = "/items";
 
-        @Autowired
-        public ItemClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
-            super(
-                    builder
-                            .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
-                            .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                            .build()
-            );
-        }
+    @Autowired
+    public ItemClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX)).requestFactory(HttpComponentsClientHttpRequestFactory::new).build());
+    }
 
-        public ResponseEntity<Object> createItem(long userId, ItemDto itemDto) {
-            return post("", userId, itemDto);
-        }
+    public ResponseEntity<Object> createItem(long userId, ItemDto itemDto) {
+        return post("", userId, itemDto);
+    }
 
     public ResponseEntity<Object> updateItem(long itemId, long userId, Item item) {
         return patch("/" + itemId, userId, item);
+    }
+
+    public ResponseEntity<Object> findItemById(long itemId, long userId) {
+        return get("/" + itemId, userId);
+    }
+
+    public ResponseEntity<Object> getItemsByUserId(long userId) {
+        return get("", userId);
+    }
+
+    public ResponseEntity<Object> findItems(String text, Integer from, Integer size, long userId) {
+        Map<String, Object> parameters = Map.of(
+                "text", text,
+                "from", from,
+                "size", size);
+        return get("/search?text={text}&from={from}&size={size}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> addComment(long userId, long itemId, CommentDto commentDto) {
+        return post("/{itemId}/comment" + itemId, userId, commentDto);
     }
 }
--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,71 +13,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-//import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.Item;
 import ru.practicum.shareit.item.dto.ItemDto;
-//import ru.practicum.shareit.item.dto.ItemDtoForOwners;
-//import ru.practicum.shareit.item.mapper.ItemMapper;
-//import ru.practicum.shareit.item.model.Item;
-//import ru.practicum.shareit.item.service.ItemService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
 @Validated
 public class ItemController {
-    private static final Logger log = LoggerFactory.getLogger(ItemController.class);
     private static final String USER_ID_HEADER = "X-Sharer-User-Id";
 
     private final ItemClient itemClient;
 
- //   private final ItemMapper itemMapper;
-
     @PostMapping
     public ResponseEntity<Object> createItem(@RequestHeader(USER_ID_HEADER) long userId, @Valid @RequestBody ItemDto itemDto) {
-        log.info("Create item");
         return itemClient.createItem(userId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
     public ResponseEntity<Object> updateItem(@RequestHeader(USER_ID_HEADER) long userId, @RequestBody Item item, @PathVariable long itemId) {
-        log.info("Update item");
-
         return itemClient.updateItem(itemId, userId, item);
     }
-//
-//    @GetMapping("/{itemId}")
-//    public ItemDtoForOwners findById(@RequestHeader(USER_ID_HEADER) long userId, @PathVariable long itemId) {
-//        log.info("Get information about item");
-//        return itemClient.findById(itemId, userId);
-//    }
-//
-//    @GetMapping
-//    public List<ItemDtoForOwners> getItemsByUserId(@RequestHeader(USER_ID_HEADER) long userId) {
-//        log.info("Get all user's items");
-//        return itemClient.getItemsByUserId(userId);
-//    }
-//
-//    @GetMapping("/search")
-//    @ResponseBody
-//    public List<ItemDto> findItems(@RequestHeader(USER_ID_HEADER) long userId, @RequestParam String text,
-//                                   @RequestParam(defaultValue = "0") @Min(0) Integer from,
-//                                   @RequestParam(defaultValue = "10") @Min(1) Integer size) {
-//
-//        log.info("Seach items by request with available status");
-//        return itemClient.findItems(text).stream()
-//                .map(itemMapper::toItemDto)
-//                .collect(Collectors.toList());
-//    }
-//
-//    @PostMapping("/{itemId}/comment")
-//    @ResponseBody
-//    public CommentDto addComment(@RequestHeader(USER_ID_HEADER) long userId, @PathVariable long itemId, @RequestBody CommentDto commentDto) {
-//        return itemMapper.toCommentDto(itemClient.addComment(userId, itemId, commentDto));
-//    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<Object> findItemById(@PathVariable long itemId, @RequestHeader(USER_ID_HEADER) long userId) {
+        return itemClient.findItemById(itemId, userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getItemsByUserId(@RequestHeader(USER_ID_HEADER) long userId) {
+        return itemClient.getItemsByUserId(userId);
+    }
+
+    @GetMapping("/search")
+    @ResponseBody
+    public ResponseEntity<Object> findItems(@RequestHeader(USER_ID_HEADER) long userId, @RequestParam String text,
+                                   @RequestParam(defaultValue = "0") @Min(0) Integer from,
+                                   @RequestParam(defaultValue = "10") @Min(1) Integer size) {
+
+        return itemClient.findItems(text, from, size, userId);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    @ResponseBody
+    public ResponseEntity<Object> addComment(@RequestHeader(USER_ID_HEADER) long userId, @PathVariable long itemId, @RequestBody CommentDto commentDto) {
+        return itemClient.addComment(userId, itemId, commentDto);
+    }
 }
